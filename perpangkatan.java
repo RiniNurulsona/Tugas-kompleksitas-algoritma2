c2a186619d2829828ac7062fb137532c6d3b22b9
Rini Nurulsona <146653197+RiniNurulsona@users.noreply.github.com>
2024-10-08 22:29:55 +0700

    Add files via upload

---
+++ perpangkatan.java
@@ -0,0 +1,10 @@
+public class perpangkatan {
+    public static  void main(String[]args){
+        int number = 5;
+        int result = number * number;
+
+        System.out.println("Hasil pangkat dua dari" + number + "adalah"+ result);
+
+    }
+        
+}
